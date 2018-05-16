a2816c56f1d242d1dceaa11d1308a522281fb685
Major Péter <majorpeter29@gmail.com>
2018-05-16 23:46:58 +0200

    initial commit (I2cInterface.h)

---
+++ I2cInterface.h
@@ -0,0 +1,35 @@
+/*
+ * I2cInterface.h
+ *
+ *  Created on: 2018. máj. 16.
+ *      Author: peti
+ */
+
+#ifndef MODULES_IFACE_I2C_I2CINTERFACE_H_
+#define MODULES_IFACE_I2C_I2CINTERFACE_H_
+
+#include <stdint.h>
+
+class I2cInterface {
+public:
+    /**
+     * write bytes to slave device
+     * @param slave_address 8-bit slave address, contains the R/nW=0 bit
+     * @param data bytes to write
+     * @param length number of bytes to write
+     * @return 0 on success
+     */
+    virtual int8_t write(uint8_t slave_address, const uint8_t* data, uint16_t length) = 0;
+
+    /**
+     * read bytes from slave device
+     * @param slave_address 8-bit slave address, contains the R/nW=1 bit
+     * @param buffer to read to
+     * @param length number of bytes to read
+     * @return 0 on success
+     */
+    virtual int8_t read(uint8_t slave_address, uint8_t* data, uint16_t length) = 0;
+    virtual ~I2cInterface() {}
+};
+
+#endif /* MODULES_IFACE_I2C_I2CINTERFACE_H_ */
